8598f20dfa8e3b9e4b8f9697aafc6384eeb276fa
Shir <Shir@DESKTOP-HRDOU18>
2019-08-01 18:59:39 +0300

    admin ws 1.8

--- CouponSystemManagment/src/coupon/system/ws/AdminService.java
+++ CouponSystemManagment/src/coupon/system/ws/AdminService.java
@@ -11,6 +11,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 
 import com.coupons.sys.beans.Company;
 import com.coupons.sys.beans.Customer;
@@ -20,7 +22,7 @@ import com.coupons.sys.clients.ClientType;
 import com.coupons.sys.clients.LoginManager;
 import com.coupons.sys.exeptions.CouponsSystemException;
 
-@Path("Admin")
+@Path("Admin-sec")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class AdminService {
@@ -33,64 +35,135 @@ private AdminFacade adminFacade;
 	}
 @Path("add-company")
 @POST
-public void addCompany(Company company) throws CouponsSystemException
+public Response addCompany(Company company) 
 {
-	this.adminFacade.addCompany(company);
+	try {
+		this.adminFacade.addCompany(company);
+		return  Response.status(Response.Status.OK).build();
+	} catch (CouponsSystemException e) {
+		e.printStackTrace();
+		return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+	}
 	}
 @Path("delete-company")
 @POST
-public void deleteCompany(int companyId) throws CouponsSystemException
+public Response deleteCompany(int companyId) 
 {
-	this.adminFacade.deleteCompany(companyId);
+	try {
+		this.adminFacade.deleteCompany(companyId);
+		return  Response.status(Response.Status.OK).build();
+	} catch (CouponsSystemException e) {
+		
+		e.printStackTrace();
+		return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+	}
 	}
 	@Path("add-customer")
 	@POST
-public void addCustomer(Customer customer) throws CouponsSystemException
+public Response addCustomer(Customer customer) 
 {
-		this.adminFacade.addCustomer(customer);
+		try {
+			this.adminFacade.addCustomer(customer);
+			return  Response.status(Response.Status.OK).build();
+		} catch (CouponsSystemException e) {
+			e.printStackTrace();
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
 	}
 	@Path("delete-customer")
 	@POST
-	public void deleteCustomer(int customerId) throws CouponsSystemException
+	public Response deleteCustomer(int customerId)
 	{
-		this.adminFacade.deleteCustomer(customerId);
+		try {
+			this.adminFacade.deleteCustomer(customerId);
+			return  Response.status(Response.Status.OK).build();
+			
+		} catch (CouponsSystemException e) {
+			
+			e.printStackTrace();
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
+		
 	}
 	@Path("get-all-companies")
 	@GET
-	public List<Company> getAllCompanies() throws CouponsSystemException
+	public Response getAllCompanies() 
 	{
-		return this.adminFacade.getAllCompanies();
+		 try {
+			this.adminFacade.getAllCompanies();
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(this.adminFacade.getAllCompanies()).build();
+		} catch (CouponsSystemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
 	}
 	@Path("get-all-customers")
 	@GET
-	public List<Customer> getAllCustomer() throws CouponsSystemException
+	public Response getAllCustomer() 
 	{
-		return this.adminFacade.getAllCustomers();
+		try {
+		
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(this.adminFacade.getAllCustomers()).build();
+		} catch (CouponsSystemException e) {
+			e.printStackTrace();
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+			
+		}
 	}
 	@Path("get-customer")
 	@GET
-	public Customer getCustomer(int customerId) throws CouponsSystemException
+	public Response getCustomer(int customerId)
 	{
-		return this.adminFacade.getOneCustomer(customerId);
+		 try {
+			this.adminFacade.getOneCustomer(customerId);
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(this.adminFacade.getAllCustomers()).build();
+		} catch (CouponsSystemException e) {
+			
+			e.printStackTrace();
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
 	}
 	
 	@Path("get-company")
 	@GET
-	public Company getCompany(int companyId) throws CouponsSystemException
+	public Response getCompany(int companyId)  
 	{
-		return this.adminFacade.getComapny(companyId);
+		try {
+			;
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(this.adminFacade.getComapny(companyId)).build();
+		} catch (CouponsSystemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
+		
 	}
 	@Path("update-company")
 	@PUT
-	public void updateCompany(Company company) throws CouponsSystemException
+	public Response updateCompany(Company company) 
 	{
-		this.adminFacade.updateCompany(company);
+		try {
+			this.adminFacade.updateCompany(company);
+			return  Response.status(Response.Status.OK).build();
+		} catch (CouponsSystemException e) {
+			e.printStackTrace();
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
+		
 	}
 	@Path("update-customer")
 	@PUT
-	public void updateCustomer(Customer customer) throws CouponsSystemException
+	public Response updateCustomer(Customer customer) 
 	{
-		this.adminFacade.updateCustomer(customer);
+		try {
+			this.adminFacade.updateCustomer(customer);
+			return  Response.status(Response.Status.OK).build();
+		} catch (CouponsSystemException e) {
+			
+			e.printStackTrace();
+			return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
 		
 	}
 	
