0a88bad605a514b24febd5111059574099e97d85
Shir <Shir@DESKTOP-HRDOU18>
2019-08-01 19:05:04 +0300

    company ws 1.8

--- CouponSystemManagment/src/coupon/system/ws/CompanyService.java
+++ CouponSystemManagment/src/coupon/system/ws/CompanyService.java
@@ -1,7 +1,5 @@
 package coupon.system.ws;
 
-import java.util.List;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,10 +8,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import com.coupons.sys.beans.Company;
 import com.coupons.sys.beans.Coupon;
-
 import com.coupons.sys.clients.ClientFacade;
 import com.coupons.sys.clients.ClientType;
 import com.coupons.sys.clients.CompanyFacade;
@@ -25,57 +22,96 @@ import com.coupons.sys.exeptions.CouponsSystemException;
 @Produces(MediaType.APPLICATION_JSON)
 public class CompanyService {
 	private CompanyFacade companyFacade;
+
 	@Path("login")
 	@POST
-		public ClientFacade login(String email, String password,ClientType clientType) throws CouponsSystemException
-		{this.companyFacade= (CompanyFacade) LoginManager.getInstance().logIn(email, password, clientType);
+	public ClientFacade login(String email, String password, ClientType clientType) throws CouponsSystemException {
+		this.companyFacade = (CompanyFacade) LoginManager.getInstance().logIn(email, password, clientType);
 		return this.companyFacade;
-		}
+	}
+
 	@Path("get-company")
 	@GET
-	public Company getComany()throws CouponsSystemException
-	{
-		return this.companyFacade.getCompany();
+	public Response getComany() {
+		try {
+			return Response.status(Response.Status.OK).entity(this.companyFacade.getCompany()).build();
+
+		} catch (Exception e) {
+
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
 	}
+
 	@Path("get-coupons")
 	@GET
-	public List <Coupon> getCoupons() throws CouponsSystemException
-	{
-	 return this.companyFacade.getCompanyCoupons();
+	public Response getCoupons() {
+		try {
+			return Response.status(Response.Status.OK).entity(this.companyFacade.getCompanyCoupons()).build();
+		} catch (CouponsSystemException e) {
+			e.printStackTrace();
+
+			return Response.status(Response.Status.OK).entity(e.getMessage()).build();
+		}
 	}
+
 	@Path("coupon-by-max-price")
 	@GET
-	public List<Coupon> getCouponsByMaxPrice(Double maxPrice) throws CouponsSystemException
-	{
-		
-		return this.companyFacade.getCompanyCoupons(maxPrice);
+	public Response getCouponsByMaxPrice(Double maxPrice) {
+
+		try {
+			return Response.status(Response.Status.OK).entity(this.companyFacade.getCompanyCoupons(maxPrice)).build();
+		} catch (CouponsSystemException e) {
+
+			return Response.status(Response.Status.OK).entity(e.getMessage()).build();
+		}
 	}
+
 	@Path("get-coupon-by-category")
 	@GET
-	public List<Coupon> GetCouponsByCategory(int categoryId) throws CouponsSystemException
-	{
-	return	this.companyFacade.getCompanyCoupons(categoryId);
+	public Response GetCouponsByCategory(int categoryId) {
+		try {
+			return Response.status(Response.Status.OK).entity(this.companyFacade.getCompanyCoupons(categoryId)).build();
+		} catch (CouponsSystemException e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.OK).entity(e.getMessage()).build();
+		}
 	}
+
 	@Path("add-coupon")
 	@POST
-	public void addCoupon(Coupon coupon) throws CouponsSystemException
-	{
-		 this.companyFacade.addCoupon(coupon);
-		 
+	public Response addCoupon(Coupon coupon) {
+		try {
+			this.companyFacade.addCoupon(coupon);
+			return Response.status(Response.Status.OK).build();
+		} catch (CouponsSystemException e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
 	}
+
 	@Path("update-coupon")
 	@PUT
-	public void updateCoupon(Coupon coupon) throws CouponsSystemException
-	{
-	 this.companyFacade.updateCoupon(coupon);
+	public Response updateCoupon(Coupon coupon) {
+		try {
+			this.companyFacade.updateCoupon(coupon);
+			return Response.status(Response.Status.OK).build();
+		} catch (CouponsSystemException e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+
+		}
 	}
+
 	@Path("delete-coupon")
 	@DELETE
-	public void deleteCoupon(int couponId) throws CouponsSystemException
-	{
-		this.companyFacade.deleteCoupon(couponId);
+	public Response deleteCoupon(int couponId) {
+		try {
+			this.companyFacade.deleteCoupon(couponId);
+			return Response.status(Response.Status.OK).build();
+		} catch (CouponsSystemException e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
 	}
-	
-	
 
 }
